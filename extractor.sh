7dcac957a208a94375fb82700790aee22f3cba00
Naategh <manamtabeshekan@gmail.com>
2019-08-31 23:06:02 +0430

    Update extractor.sh

--- extractor.sh
+++ extractor.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-	echo "Usage: bash $0 /path/to/file"
+if [ $# -eq 0 ]; then
+	echo "Usage: bash $0 test.zip example.tar ..."
 	exit 0
 fi
 
-FILE=$1
+FILE=$@
 
 extract () {
 	# We can simply use from dtrx tool to extract any archived file with any extension :)
 	# Check dtrx installed or not
 	if hash dtrx 2> /dev/null; then
-		dtrx $1
+		dtrx $@
 	else
 		echo "Please install dtrx: sudo apt-get install dtrx"
 		#exit 0
