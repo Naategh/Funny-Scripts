70a54126723147d0b753ca5cfe0d77d1e8aa9c71
Naategh <manamtabeshekan@gmail.com>
2019-09-01 20:45:56 +0430

    Add check availability of toilet

--- colorful-date.sh
+++ colorful-date.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "Colorful Date :)" | toilet -f mini --gay
-sleep 0.9
-while true; do
-  echo "$(date '+%D %T' | toilet -f term -F border --gay)"
-  sleep 1
-done
+if hash toilet 2> /dev/null; then
+	echo "Colorful Date :)" | toilet -f mini --gay
+	sleep 0.9
+	while true; do
+	  echo "$(date '+%D %T' | toilet -f term -F border --gay)"
+	  sleep 1
+	done
+else
+	echo "Please install toilet: sudo apt-get install toilet"
+	exit 0
+fi
