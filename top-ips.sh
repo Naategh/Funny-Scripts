039124dd869aa8beb97a1046229a672e82bfa418
Naategh <manamtabeshekan@gmail.com>
2019-09-01 20:52:02 +0430

    Change var names

--- top-ips.sh
+++ top-ips.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-read -p "Domain name: " DOMAIN
-read -p "Where to save final report? " SAVE
-read -p "Path to web server log file: " LOGFILE
+read -p "Domain name: " domain
+read -p "Where to save final report? " save
+read -p "Path to web server log file: " logFile
 
-[ ! -d $SAVE ] && mkdir -p $SAVE
+[ ! -d $save ] && mkdir -p $save
 
-if [[ -f $LOGFILE ]]; then
-	echo "Processing log for $DOMAIN ..."
-	touch $SAVE/$DOMAIN.txt
-	awk '{ print $1}' $LOGFILE | sort  | uniq -c  | sort -nr > $SAVE/$DOMAIN.txt
-	echo "Report written to $SAVE/$DOMAIN.txt"
+if [[ -f $logFile ]]; then
+	echo "Processing log for $domain ..."
+	touch $save/$domain.txt
+	awk '{ print $1}' $logFile | sort  | uniq -c  | sort -nr > $save/$domain.txt
+	echo "Report written to $save/$domain.txt"
 fi
